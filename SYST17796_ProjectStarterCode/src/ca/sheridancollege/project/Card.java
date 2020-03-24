9c2668ff4049240acaa5f094c0493227c4ae0a0c
omerm <omerm@LAPTOP-J94Q0CHU>
2020-03-24 11:55:15 -0400

    Changed up comments for testing

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
@@ -1,7 +1,7 @@
 /**
  * SYST 17796 Project Winter 2020 Base code.
  * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
+ * Omer Siddiqui
  */
 package ca.sheridancollege.project;
 
